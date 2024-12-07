d1c8ceefe9265a3fcabcb4ab1738f391ed65c35f
Akbar1712 <akbartursunaliev17@gmail.com>
2024-12-07 19:52:10 +0600

    акбар: изменил будущее

---
+++ module_baiel.py
@@ -0,0 +1,17 @@
+STUDENTS_LIST = [
+                 'ADILET',
+                 'ESEN M.',
+                 'AZAMAT T.',
+                 'KYLYCH S.',
+                 'ARSEN K.',
+                 ]
+
+MAX_SCORE = 30
+
+STUDENTS_GRADES = (
+    (15, MAX_SCORE),
+    (12, MAX_SCORE),
+    (16, MAX_SCORE),
+    (20, MAX_SCORE),
+    (23, MAX_SCORE),
+)
